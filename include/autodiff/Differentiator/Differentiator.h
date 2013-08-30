df550bf551d75df51ba61d8a1220a9959d8646ae
v.g.vassilev@gmail.com <v.g.vassilev@gmail.com@1c11bbf4-e5e0-636f-9836-5d70ccbefa43>
2013-08-30 21:52:19 +0000

    Check in the working version of the interfaces. This should improve the running of the testsuite.

--- include/autodiff/Differentiator/Differentiator.h
+++ include/autodiff/Differentiator/Differentiator.h
@@ -17,19 +17,53 @@
 
 #include "BuiltinDerivatives.h"
 
+extern "C" int printf(const char* fmt, ...);
+
 template<typename ReturnResult, typename... ArgsTypes>
 class Function {
 public:
   using FunctionType = ReturnResult (*)(ArgsTypes...);
 private:
   FunctionType m_Function;
+  const char m_Code[];
 public:
+  Function(FunctionType f)
+    : m_Function(f), m_Code("aaaa") { }
+
   template<typename... Args>
   Function differentiate(Args&&... args);
 
   template<typename... Args>
   ReturnResult execute(Args&&... args) {
-    return m_Function(args...);
+    if(!m_Function)
+      printf("Function ptr must be set.");
+    printf("I was here. m_Function=%p\n", m_Function);
+    ReturnResult result = m_Function(args...);
+    return result;
+  }
+
+  // template<typename... Args>
+  // void executeAndPrintResult(Args&&... args) {
+  //   execute(args...);
+  //   printf("Returns void\n");
+  // }
+
+  // template<typename... Args>
+  // int executeAndPrintResult(Args&&... args) {
+  //   ReturnResult result = execute(args...);
+  //   printf("Returns covertible to int %d\n", result);
+  //   return result;
+  // }
+
+  // template<typename... Args>
+  // double executeAndPrintResult(Args&&... args) {
+  //   ReturnResult result = execute(args...);
+  //   printf("Returns covertible to double %f\n", result);
+  //   return result;
+  // }
+
+  void dump() const {
+    printf("The code is: %s\n", m_Code);
   }
 };
 
@@ -47,9 +81,7 @@ public:
 // ok 03.06.2013)
 
 template<typename F, typename... Args, typename... A>
-Function<F> diff(F (*f)(Args...), A&&... a) {
-  
-  //return f(a...);
-  return Function<F>();
+Function<F, Args...> diff(F (*f)(Args...), A&&... a) {
+  return Function<F, Args...>(f);
 }
 #endif // AUTODIFF_DIFFERENTIATOR
