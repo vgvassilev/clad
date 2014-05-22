416885b23f10b91249bacdf4da72a9b6681fe430
Martin Vassilev <mrtn.vassilev@gmail.com>
2014-05-23 00:22:29 +0200

    Synchronize error messages with the Diagnostics system.

--- test/FirstDerivative/FunctionsOneVariable.C
+++ test/FirstDerivative/FunctionsOneVariable.C
@@ -14,18 +14,19 @@ int f_simple(int x) {
 
 int main () {
   int x = 4;
-  diff(f_simple, x); // expected-error {{Must be IntegerLiteral}}
+  diff(f_simple, x); // expected-error {{Must be an integral value}}
   // Here the second arg denotes the differentiation of f with respect to the
   // given arg.
   diff(f_simple, 2); // expected-error {{Invalid argument index 2 among 1 argument(s)}}
+  diff(f_simple, -1); // expected-error {{Invalid argument index -1 among 1 argument(s)}}
   diff(f_simple, 1);
   
   //long y = 2;
   //diff(g, y);
+  // FIXME:
   // Would it be better if the signature is diff(f, 1), where 1 is the number
   // of the respected parameter? This will help to get rid of the variadic
-  // templates (C++11) feature?
+  // templates (C++11) feature? diff(g, 2);
   
-  //diff(g, 2);
   return 0;
 }
