10db69f5dff3f8adfa36799de7ef1d2366b24096
Martin Vassilev <mrtn.vassilev@gmail.com>
2014-05-31 13:54:41 +0200

    Upgrade warning into error. Surround the names with ticks.

--- test/FirstDerivative/FunctionCalls.C
+++ test/FirstDerivative/FunctionCalls.C
@@ -2,8 +2,8 @@
 
 #include "clad/Differentiator/Differentiator.h"
 
-int printf(const char* fmt, ...); // expected-warning {{function printf was not differentiated because it is not declared in namespace custom_derivatives}}
-int no_body(int x); // expected-error {{attempted differention of function no_body, which does not have a definition}}
+int printf(const char* fmt, ...); // expected-warning {{function 'printf' was not differentiated because it is not declared in namespace 'custom_derivatives'}}
+int no_body(int x); // expected-error {{attempted differention of function 'no_body', which does not have a definition}}
 int custom_fn(int x);
 int custom_fn(float x);
 int custom_fn();
@@ -38,7 +38,7 @@ int overloaded(float x) {
 
 int overloaded() {
   return 3;
-} // expected-warning {{function overloaded was not differentiated because it is not declared in namespace custom_derivatives}}
+} // expected-warning {{function 'overloaded' was not differentiated because it is not declared in namespace 'custom_derivatives'}}
 
 float test_1(int x) {
   return overloaded(x) + custom_fn(x);
