be88f672e69c309e278683c715d8e86cf7d26408
Martin Vassilev <mrtn.vassilev@gmail.com>
2014-06-15 15:57:52 +0200

    Adapt to the new interfaces.

--- test/FirstDerivative/TemplateFunction.C
+++ test/FirstDerivative/TemplateFunction.C
@@ -20,47 +20,47 @@ T multiplication(T x) {
 int main () {
   int x;
 
-  diff(simple_return<int>, 1);
+  clad::differentiate(simple_return<int>, 1);
   // CHECK: int simple_return_derived_x(int x) {
   // CHECK-NEXT: return 1;
   // CHECK-NEXT: }
 
-  diff(simple_return<float>, 1);
+  clad::differentiate(simple_return<float>, 1);
   // CHECK: float simple_return_derived_x(float x) {
   // CHECK-NEXT: return 1;
   // CHECK-NEXT: }
 
-  diff(simple_return<double>, 1);
+  clad::differentiate(simple_return<double>, 1);
   // CHECK: double simple_return_derived_x(double x) {
   // CHECK-NEXT: return 1;
   // CHECK-NEXT: }
 
-  diff(addition<int>, 1);
+  clad::differentiate(addition<int>, 1);
   // CHECK: int addition_derived_x(int x) {
   // CHECK-NEXT: return 1 + (1);
   // CHECK-NEXT: }
 
-  diff(addition<float>, 1);
+  clad::differentiate(addition<float>, 1);
   // CHECK: float addition_derived_x(float x) {
   // CHECK-NEXT: return 1 + (1);
   // CHECK-NEXT: }
 
-  diff(addition<double>, 1);
+  clad::differentiate(addition<double>, 1);
   // CHECK: double addition_derived_x(double x) {
   // CHECK-NEXT: return 1 + (1);
   // CHECK-NEXT: }
 
-  diff(multiplication<int>, 1);
+  clad::differentiate(multiplication<int>, 1);
   // CHECK: int multiplication_derived_x(int x) {
   // CHECK-NEXT: return (1 * x + x * 1);
   // CHECK-NEXT: }
 
-  diff(multiplication<float>, 1);
+  clad::differentiate(multiplication<float>, 1);
   // CHECK: float multiplication_derived_x(float x) {
   // CHECK-NEXT: return (1 * x + x * 1);
   // CHECK-NEXT: }
 
-  diff(multiplication<double>, 1);
+  clad::differentiate(multiplication<double>, 1);
   // CHECK: double multiplication_derived_x(double x) {
   // CHECK-NEXT: return (1 * x + x * 1);
   // CHECK-NEXT: }
