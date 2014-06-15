cc57787838d3987b155fd737c35a3efdcd8b1ac6
Vassil Vassilev <vvasilev@cern.ch>
2014-06-15 17:12:20 +0200

    Add another demo showing how to debug clad's outputd.

---
+++ demos/DebuggingClad.C
@@ -0,0 +1,39 @@
+//--------------------------------------------------------------------*- C++ -*-
+// clad - The C++ Clang-based Automatic Differentiator
+//
+// A demo, describing a basic usage of the tool.
+//
+// author:  Vassil Vassilev <vvasilev-at-cern.ch>
+//----------------------------------------------------------------------------//
+
+// To run the demo please type:
+// path/to/clang  -Xclang -add-plugin -Xclang clad -Xclang -load -Xclang \
+// path/to/libclad.so  -I../include/ -x c++ -std=c++11 DebuggingClad.C
+//
+// A typical invocation would be:
+// ../../../../obj/Debug+Asserts/bin/clang  -Xclang -add-plugin -Xclang clad \
+// -Xclang -load -Xclang ../../../../obj/Debug+Asserts/lib/libclad.dylib     \
+// -I../include/ -x c++ -std=c++11 DebuggingClad.C
+
+// Necessary for clad to work include
+#include "clad/Differentiator/Differentiator.h"
+
+float myFunc(float x) {
+   return 3.14 * x * x;
+}
+
+int main() {
+  // In some cases one needs to make sure the produced code is correct. After
+  // the differentiation process finishes, dump can be called to print out some
+  // debug information.
+  auto cladMyFunc = clad::differentiate(myFunc, 1);
+  cladMyFunc.dump();
+
+  // At runtime, this would produce an output similar to:
+  // float myFunc_derived_x(float x) {
+  //    return ((3.1400000000000001 * x + 3.1400000000000001 * 1) * x + 3.1400000000000001 * x * 1);
+  // }
+  //
+
+  return 0;
+}
