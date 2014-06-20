3a5439c725224cb7fb715961d1a806332d147445
Vassil Vassilev <vvasilev@cern.ch>
2014-06-20 10:56:39 +0200

    Add the demos to the testsuite.

---
+++ test/Misc/RunDemos.C
@@ -0,0 +1,4 @@
+// RUN: %cladclang %S/../../demos/BasicUsage.cpp -I%S/../../include -fsyntax-only 2>&1
+// RUN: %cladclang %S/../../demos/ControlFlow.cpp -I%S/../../include -fsyntax-only 2>&1
+// RUN: %cladclang %S/../../demos/DebuggingClad.cpp -I%S/../../include -fsyntax-only 2>&1
+// RUN: %cladclang %S/../../demos/Gradient.cpp -I%S/../../include -fsyntax-only 2>&1
