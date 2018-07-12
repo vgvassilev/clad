402b899de708687740309c3a4c166ce0568d846d
E. G. Patrick Bos <egpbos@gmail.com>
2018-07-12 15:49:34 +0200

    First rough implementation with RooGaussian

--- demos/Gradient_RooFit.cpp
+++ demos/Gradient_RooFit.cpp
@@ -5,42 +5,43 @@
 // implicit function.
 //
 // author:  Alexander Penev <alexander_penev-at-yahoo.com>
+// author:  Patrick Bos <p.bos-at-esciencecenter.nl>
 //----------------------------------------------------------------------------//
 
 // To run the demo please type:
 // path/to/clang  -Xclang -add-plugin -Xclang clad -Xclang -load -Xclang \
-// path/to/libclad.so  -I../include/ -x c++ -std=c++11 Gradient.cpp
+// path/to/libclad.so  -I../include/ -I $ROOTSYS/include -x c++ -std=c++11 \
+// Gradient_RooFit.cpp -o Gradient_RooFit
 //
 // A typical invocation would be:
 // ../../../../obj/Debug+Asserts/bin/clang  -Xclang -add-plugin -Xclang clad \
 // -Xclang -load -Xclang ../../../../obj/Debug+Asserts/lib/libclad.dylib     \
-// -I../include/ -x c++ -std=c++11 Gradient.cpp
+// -I../include/ -I $ROOTSYS/include -x c++ -std=c++11 Gradient_RooFit.cpp \
+// -o Gradient_RooFit
 
 // Necessary for clad to work include
 #include "clad/Differentiator/Differentiator.h"
 
+#include "RooRealVar.h"
+#include "RooConstVar.h"
+#include "RooGaussian.h"
+
 // Implicit function for sphere
-float sphere_implicit_func(float x, float y, float z, float xc, float yc, float zc, float r) {
-  return (x-xc)*(x-xc) + (y-yc)*(y-yc) + (z-zc)*(z-zc) - r*r;
+float roofit_gauss(float _x) {
+  RooRealVar x("x", "x", _x);
+  RooConstVar mean("mean", "Mean of Gaussian", 0) ;
+  RooConstVar sigma("sigma","Width of Gaussian",3) ;
+  RooGaussian gauss("gauss","gauss(x,mean,sigma)", x, mean, sigma) ;
+
+  return gauss.GetVal();
 }
 
 int main() {
-  // Differentiate implicit sphere function. Clad will produce the three partial derivatives
-  // of function sphere_implicit_func
-  auto sphere_implicit_func_dx = clad::differentiate(sphere_implicit_func, 0);
-  auto sphere_implicit_func_dy = clad::differentiate(sphere_implicit_func, 1);
-  auto sphere_implicit_func_dz = clad::differentiate(sphere_implicit_func, 2);
-
-  // Point P=(x,y,z) on surface
-  float x = 5.0f;
-  float y = 0;
-  float z = 0;
+  // Differentiate implicit sphere function. Clad will produce the three derivatives
+  // of function roofit_gauss
+  auto roofit_gauss_dx = clad::differentiate(roofit_gauss, 0);
 
   // Calculate gradient in point P (Normal vector)
-  float Nx = sphere_implicit_func_dx.execute(x, y, z, 0, 0, 0, 5.0f);
-  float Ny = sphere_implicit_func_dy.execute(x, y, z, 0, 0, 0, 5.0f);
-  float Nz = sphere_implicit_func_dz.execute(x, y, z, 0, 0, 0, 5.0f);
-  printf("Result is N=(%f,%f,%f)\n", Nx, Ny, Nz);
-
-  return 0;
+  float grad_0 = roofit_gauss_dx.execute(0);
+  printf("Results are %f, \n", grad_0);
 }
