f630f8f90fa808e1bf2935015f3bab24966c1156
Alexander Penev <alexander_penev@yahoo.com>
2019-11-05 16:20:50 +0200

    Enable clad for clang-5, clang-6 and clang-7.
    Some changes are made in cmake files to more flexible discover compatible
    clang and llvm. If automatic cmake detection not work correctly Clang_DIR
    and LLVM_DIR may be defined to select right instalation or build folders
    for Clang and LLVM. Selection by version number for now do not work properly
    due bug in the Clang cmake files.
    
    A compatibility layer has been added to support various versions of clang.

--- tools/ClangPlugin.cpp
+++ tools/ClangPlugin.cpp
@@ -183,7 +183,7 @@ namespace clad {
       DeclarationName Name = &C.Idents.get("clad");
       Sema &SemaR = m_CI.getSema();
       LookupResult R(SemaR, Name, SourceLocation(), Sema::LookupNamespaceName,
-                     Sema::ForVisibleRedeclaration);
+                     clad_compat::Sema_ForVisibleRedeclaration);
       SemaR.LookupQualifiedName(R, C.getTranslationUnitDecl(),
                                 /*allowBuiltinCreation*/ false);
       m_HasRuntime = !R.empty();
