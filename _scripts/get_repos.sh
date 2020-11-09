fedf9c393401f4a1c6a62c5384dbc0f8d8d11242
David Lange <david.lange@cern.ch>
2020-11-09 17:21:15 +0100

    commands to update repo contributors

---
+++ _scripts/get_repos.sh
@@ -0,0 +1,3 @@
+wget -O cling_repo.json https://api.github.com/repos/root-project/cling/contributors
+wget -O clad_repo.json https://api.github.com/repos/vgvassilev/clad/contributors
+wget -O comp_res_web_repo.json https://api.github.com/repos/compiler-research/compiler-research.github.io/contributors 
