5d765d53a6c4f38bfdf9ec686016c1989c275ca3
Parth <partharora99160808@gmail.com>
2021-05-25 15:28:16 +0530

    Fix for one specialization

--- include/clad/Differentiator/FunctionTraits.h
+++ include/clad/Differentiator/FunctionTraits.h
@@ -239,11 +239,15 @@ namespace clad {
   };
 
   // specializations for member functions pointer types with no cv-qualifiers
+  template <class C, class... Args> 
+  struct ExtractDerivedFnTraits<void (C::*)(Args...)> { 
+    using type = void (C::*)(Args...); 
+  };
+  
   template <class ReturnType, class C, class... Args> 
   struct ExtractDerivedFnTraits<ReturnType (C::*)(Args...)> { 
     using type = void (C::*)(Args..., ReturnType*); 
   };
-  
   // specializations for member functions pointer types with only cv-qualifiers
   template <class ReturnType, class C, class... Args> 
   struct ExtractDerivedFnTraits<ReturnType (C::*)(Args...) const> { 
