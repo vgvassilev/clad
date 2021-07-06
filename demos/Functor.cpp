ef9299b465b873507738c5caa433c5eb0bbbb047
Parth <partharora99160808@gmail.com>
2021-07-06 17:45:06 +0530

    Add setObject and clearObject use cases in Functors demo

--- demos/Functor.cpp
+++ demos/Functor.cpp
@@ -36,19 +36,59 @@ int main() {
   Equation E(3, 5);
 
   // Functor is an object of any type which have user defined call operator.
+  //
   // Clad differentiation functions can directly differentiate functors.
-  // Functors can be passed to clad differentiation functions in two distinct ways:
+  // Functors can be passed to clad differentiation functions in two distinct
+  // ways:
 
   // 1) Pass by reference
   // differentiates `E` wrt parameter `i`
+  // object `E` is saved in the `CladFunction` object `d_E`
   auto d_E = clad::differentiate(E, "i");
 
   // 2) Pass as pointers
   // differentiates `E` wrt parameter `i`
+  // object `E` is saved in the `CladFunction` object `d_E_pointer`
   auto d_E_pointer = clad::differentiate(&E, "i");
 
   // calculate differentiation of `E` when (i, j) = (7, 9)
   double res1 = d_E.execute(7, 9);
   double res2 = d_E_pointer.execute(7, 9);
+  printf("%.2f %.2f", res1, res2);
+
+  Equation AnotherE(11, 13);
+
+  // If differentiation of `E` is not needed, then `d_E` and `d_E_pointer` can
+  // be reused for computing differentiation of another functor of the same
+  // type by modifying the saved object in `CladFunction`. This can save
+  // computation time.
+  d_E.setObject(&AnotherE);
+  d_E_pointer.setObject(&AnotherE);
+
+  // calculate differentiation of `AnotherE` when (i, j) = (7, 9)
+  res1 = d_E.execute(7, 9);
+  res2 = d_E_pointer.execute(7, 9);
+  printf("%.2f %.2f", res1, res2);
+
+  // Differentiation of any other functor of the same type can also be computed
+  // without changing the saved object in `CladFunction` object by explicitly
+  // passing the functor as the first argument while calling
+  // `CladFunction::execute`.
+  // Calculates differentiation of `E` when (i, j) = (7, 9)
+  res1 = d_E.execute(E, 7, 9);
+  res2 = d_E_pointer.execute(E, 7, 9);
+  printf("%.2f %.2f", res1, res2);
+
+  // Saved object in `CladFunction` object can be removed using
+  // `CladFunction::clearObject`
+  d_E.clearObject();
+  d_E_pointer.clearObject();
+
+  // Now it is necessary to explicitly pass the functor object while calling
+  // `CladFunction::execute`, since no saved object is available.
+  // Calculates differentiation of `E` when (i, j) = (7, 9)
+  res1 = d_E.execute(E, 7, 9);
+  // Calculates differentiation of `AnotherE` when (i, j) = (7, 9)
+  res2 = d_E_pointer.execute(AnotherE, 7, 9);
   printf("%.2f %.2f", res1, res2);
 }
