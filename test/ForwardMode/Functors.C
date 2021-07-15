e44b374e3dcf38218fbefaa0e66cf92844901f77
Parth <partharora99160808@gmail.com>
2021-07-15 23:09:02 +0530

    Fix building of nested name specifiers in getArg function
    This commit modifies code in getArg function to build correct and
    complete nested name specifiers for the call operator.
    It also fixes the failing asserts in clang debug build on running tests for
    differentiating functors. Credit goes to @grimmmyshini and @sudo-panda for
    bringing this error to attention.
    
    The root cause of the failing asserts was lambda types not having any
    associated identifiers. This was causing issues while creating correct
    nested name specifiers for the call operator.

--- test/ForwardMode/Functors.C
+++ test/ForwardMode/Functors.C
@@ -100,6 +100,10 @@ namespace outer {
       // CHECK-NEXT:     return (0. * i + _t0 * _d_i) * j + _t1 * _d_j;
       // CHECK-NEXT: }   
     };
+
+    auto lambdaNNS = [](double i, double j) {
+      return i*i*j;
+    };
   }
 }
 
@@ -114,10 +118,12 @@ double x=3;
 
 int main() {
   Experiment E(3, 5);
+  auto E_Again = E;
   const ExperimentConst E_Const(3, 5);
   volatile ExperimentVolatile E_Volatile(3, 5);
   const volatile ExperimentConstVolatile E_ConstVolatile(3, 5);
   outer::inner::ExperimentNNS E_NNS(3, 5);
+  auto E_NNS_Again = E_NNS;
   auto lambda = [](double i, double j) {
     return i*i*j;
   };
@@ -140,33 +146,52 @@ int main() {
   // CHECK-NEXT:     return (0 * i + x * _d_i) * jj + _t0 * _d_jj;
   // CHECK-NEXT: }
 
+  auto lambdaNNS = outer::inner::lambdaNNS;
+
+  // CHECK: inline double operator_call_darg0(double i, double j) const {
+  // CHECK-NEXT:     double _d_i = 1;
+  // CHECK-NEXT:     double _d_j = 0;
+  // CHECK-NEXT:     double _t0 = i * i;
+  // CHECK-NEXT:     return (_d_i * i + i * _d_i) * j + _t0 * _d_j;
+  // CHECK-NEXT: }
+
   INIT(E);
+  INIT(E_Again);
   INIT(E_Const);
   INIT(E_Volatile);
   INIT(E_ConstVolatile);
   INIT(E_NNS);
+  INIT(E_NNS_Again);
   INIT(lambda);
   INIT(lambdaWithCapture);
+  INIT(lambdaNNS);
 
-  TEST(E);                // CHECK-EXEC: 27.00 27.00
-  TEST(E_Const);          // CHECK-EXEC: 27.00 27.00
-  TEST(E_Volatile);       // CHECK-EXEC: 27.00 27.00
-  TEST(E_ConstVolatile);  // CHECK-EXEC: 27.00 27.00
-  TEST(E_NNS);            // CHECK-EXEC: 27.00 27.00
-  TEST(lambda);           // CHECK-EXEC: 126.00 126.00
-  TEST(lambdaWithCapture);// CHECK-EXEC: 27.00 27.00
+  TEST(E);                  // CHECK-EXEC: 27.00 27.00
+  TEST(E_Again);            // CHECK-EXEC: 27.00 27.00
+  TEST(E_Const);            // CHECK-EXEC: 27.00 27.00
+  TEST(E_Volatile);         // CHECK-EXEC: 27.00 27.00
+  TEST(E_ConstVolatile);    // CHECK-EXEC: 27.00 27.00
+  TEST(E_NNS);              // CHECK-EXEC: 27.00 27.00
+  TEST(E_NNS_Again);        // CHECK-EXEC: 27.00 27.00
+  TEST(lambda);             // CHECK-EXEC: 126.00 126.00
+  TEST(lambdaWithCapture);  // CHECK-EXEC: 27.00 27.00
+  TEST(lambdaNNS);          // CHECK-EXEC: 126.00 126.00
 
   E.setX(6);
+  E_Again.setX(6);
   E_Const.setX(6);
   E_Volatile.setX(6);
   E_ConstVolatile.setX(6);
   E_NNS.setX(6);
+  E_NNS_Again.setX(6);
   x = 6;
 
-  TEST(E);                // CHECK-EXEC: 54.00 54.00
-  TEST(E_Const);          // CHECK-EXEC: 54.00 54.00
-  TEST(E_Volatile);       // CHECK-EXEC: 54.00 54.00
-  TEST(E_ConstVolatile);  // CHECK-EXEC: 54.00 54.00
-  TEST(E_NNS);            // CHECK-EXEC: 54.00 54.00
-  TEST(lambdaWithCapture);// CHECK-EXEC: 54.00 54.00
+  TEST(E);                  // CHECK-EXEC: 54.00 54.00
+  TEST(E_Again);            // CHECK-EXEC: 54.00 54.00
+  TEST(E_Const);            // CHECK-EXEC: 54.00 54.00
+  TEST(E_Volatile);         // CHECK-EXEC: 54.00 54.00
+  TEST(E_ConstVolatile);    // CHECK-EXEC: 54.00 54.00
+  TEST(E_NNS);              // CHECK-EXEC: 54.00 54.00
+  TEST(E_NNS_Again);        // CHECK-EXEC: 54.00 54.00
+  TEST(lambdaWithCapture);  // CHECK-EXEC: 54.00 54.00
 }
