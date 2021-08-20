4ae80a61e755b80129e613db436ba68960189837
Baidyanath Kundu <kundubaidya99@gmail.com>
2021-08-20 13:57:21 +0530

    Modify MoveData in Tape.h to make it CUDA compatible
    * Add addressof function make the call to std::address CUDA compatible
    * Change fprintf to printf
    * Add trap function to replace exit function

--- include/clad/Differentiator/CladConfig.h
+++ include/clad/Differentiator/CladConfig.h
@@ -3,10 +3,46 @@
 #ifndef CLAD_CONFIG_H
 #define CLAD_CONFIG_H
 
+#include <cstdlib>
+#include <memory>
+
+// Define CUDA_HOST_DEVICE attribute for adding CUDA support to
+// clad functions
 #ifdef __CUDACC__
 #define CUDA_HOST_DEVICE __host__ __device__
 #else
 #define CUDA_HOST_DEVICE
+#endif
+
+// Define trap function that is a CUDA compatible replacement for
+// exit(int code) function
+#ifdef __CUDACC__
+__device__ void trap(int code) {
+  asm("trap;");
+}
+__host__ void trap(int code) {
+  exit(code);
+}
+#else
+void trap(int code) {
+  exit(code);
+}
+#endif
+
+#ifdef  __CUDACC__
+template<typename T>
+__device__ T* addressof(T& r) {
+  return __builtin_addressof(r);
+}
+template<typename T>
+__host__ T* addressof(T& r) {
+  return std::addressof(r);
+}
+#else
+template<typename T>
+T* addressof(T& r) {
+  return std::addressof(r);
+}
 #endif
 
 #endif // CLAD_CONFIG_H
