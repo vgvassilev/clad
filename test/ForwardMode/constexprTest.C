c83245e42cba77e02f46fbd4a4e140d90c0d5a6a
Ris-Bali <rishabhsbali@gmail.com>
2023-04-18 17:52:41 +0530

    Add tests for differentiation of constexpr functions
    Resolves #437. This PR adds tests for differentiation of constexpr functions for all the four modes available in clad.

---
+++ test/ForwardMode/constexprTest.C
@@ -0,0 +1,43 @@
+// RUN: %cladclang -lm -lstdc++ %s -I%S/../../include -oconstexprTest.out | FileCheck %s
+// RUN: ./constexprTest.out | FileCheck -check-prefix=CHECK-EXEC %s
+
+#include "clad/Differentiator/Differentiator.h"
+
+#include "../TestUtils.h"
+
+
+constexpr double fn(double a, double b) {
+    return (a+b)/2;
+}
+
+//CHECK: constexpr double fn_darg0(double a, double b) {
+//CHECK-NEXT:    double _d_a = 1;
+//CHECK-NEXT:    double _d_b = 0;
+//CHECK-NEXT:    double _t0 = (a + b);
+//CHECK-NEXT:    return ((_d_a + _d_b) * 2 - _t0 * 0) / (2 * 2);
+//CHECK-NEXT:}
+
+constexpr double mul(double a, double b, double c) {
+     double val = 99.00;
+     double result = val * a + 100 - b + c;
+     return result;
+}
+
+//CHECK: constexpr double mul_darg0(double a, double b, double c) {
+//CHECK-NEXT:    double _d_a = 1;
+//CHECK-NEXT:    double _d_b = 0;
+//CHECK-NEXT:    double _d_c = 0;
+//CHECK-NEXT:    double _d_val = 0.;
+//CHECK-NEXT:    double val = 99.;
+//CHECK-NEXT:    double _d_result = _d_val * a + val * _d_a + 0 - _d_b + _d_c;
+//CHECK-NEXT:    double result = val * a + 100 - b + c;
+//CHECK-NEXT:    return _d_result;
+//CHECK-NEXT:}
+
+int main() {
+    INIT_DIFFERENTIATE(fn,"a");
+    INIT_DIFFERENTIATE(mul, "a");
+
+    TEST_DIFFERENTIATE(fn, 4, 7); // CHECK-EXEC: {0.50}
+    TEST_DIFFERENTIATE(mul, 5, 6, 10); // CHECK-EXEC: {99.00}
+}
---
+++ test/Gradient/constexprTest.C
@@ -0,0 +1,93 @@
+// RUN: %cladclang -lm -lstdc++ %s -I%S/../../include -oconstexprTest.out | FileCheck %s
+// RUN: ./constexprTest.out | FileCheck -check-prefix=CHECK-EXEC %s
+
+#include "clad/Differentiator/Differentiator.h"
+
+#include "../TestUtils.h"
+
+constexpr double mul (double a, double b, double c) {
+    double result = a*b*c;
+    return result;
+}
+
+//CHECK: constexpr void mul_grad(double a, double b, double c, clad::array_ref<double> _d_a, clad::array_ref<double> _d_b, clad::array_ref<double> _d_c) {
+//CHECK-NEXT:    double _t0;
+//CHECK-NEXT:    double _t1;
+//CHECK-NEXT:    double _t2;
+//CHECK-NEXT:    double _t3;
+//CHECK-NEXT:    double _d_result = 0;
+//CHECK-NEXT:    _t2 = a;
+//CHECK-NEXT:    _t1 = b;
+//CHECK-NEXT:    _t3 = _t2 * _t1;
+//CHECK-NEXT:    _t0 = c;
+//CHECK-NEXT:    double result = _t3 * _t0;
+//CHECK-NEXT:    double mul_return = result;
+//CHECK-NEXT:    goto _label0;
+//CHECK-NEXT:  _label0:
+//CHECK-NEXT:    _d_result += 1;
+//CHECK-NEXT:    {
+//CHECK-NEXT:        double _r0 = _d_result * _t0;
+//CHECK-NEXT:        double _r1 = _r0 * _t1;
+//CHECK-NEXT:        * _d_a += _r1;
+//CHECK-NEXT:        double _r2 = _t2 * _r0;
+//CHECK-NEXT:        * _d_b += _r2;
+//CHECK-NEXT:        double _r3 = _t3 * _d_result;
+//CHECK-NEXT:        * _d_c += _r3;
+//CHECK-NEXT:    }
+//CHECK-NEXT:}
+
+constexpr double fn( double a, double b, double c) {
+    double val = 98.00;
+    double result = a * b / c * (a+b) * 100 +c;
+    return result;
+}
+
+//CHECK: constexpr void fn_grad(double a, double b, double c, clad::array_ref<double> _d_a, clad::array_ref<double> _d_b, clad::array_ref<double> _d_c) {
+//CHECK-NEXT:    double _d_val = 0;
+//CHECK-NEXT:    double _t0;
+//CHECK-NEXT:    double _t1;
+//CHECK-NEXT:    double _t2;
+//CHECK-NEXT:    double _t3;
+//CHECK-NEXT:    double _t4;
+//CHECK-NEXT:    double _t5;
+//CHECK-NEXT:    double _d_result = 0;
+//CHECK-NEXT:    double val = 98.;
+//CHECK-NEXT:    _t3 = a;
+//CHECK-NEXT:    _t2 = b;
+//CHECK-NEXT:    _t4 = _t3 * _t2;
+//CHECK-NEXT:    _t1 = c;
+//CHECK-NEXT:    _t5 = _t4 / _t1;
+//CHECK-NEXT:    _t0 = (a + b);
+//CHECK-NEXT:    double result = _t5 * _t0 * 100 + c;
+//CHECK-NEXT:    double fn_return = result;
+//CHECK-NEXT:    goto _label0;
+//CHECK-NEXT:  _label0:
+//CHECK-NEXT:    _d_result += 1;
+//CHECK-NEXT:    {
+//CHECK-NEXT:        double _r0 = _d_result * 100;
+//CHECK-NEXT:        double _r1 = _r0 * _t0;
+//CHECK-NEXT:        double _r2 = _r1 / _t1;
+//CHECK-NEXT:        double _r3 = _r2 * _t2;
+//CHECK-NEXT:        * _d_a += _r3;
+//CHECK-NEXT:        double _r4 = _t3 * _r2;
+//CHECK-NEXT:        * _d_b += _r4;
+//CHECK-NEXT:        double _r5 = _r1 * -_t4 / (_t1 * _t1);
+//CHECK-NEXT:        * _d_c += _r5;
+//CHECK-NEXT:        double _r6 = _t5 * _r0;
+//CHECK-NEXT:        * _d_a += _r6;
+//CHECK-NEXT:        * _d_b += _r6;
+//CHECK-NEXT:        * _d_c += _d_result;
+//CHECK-NEXT:    }
+//CHECK-NEXT:}
+
+double arr[3] = {};
+double arr1[3] = {};
+int main() {
+
+    INIT_GRADIENT(mul);
+    INIT_GRADIENT(fn);
+
+    TEST_GRADIENT(mul, 3, 2, 3, 4, &arr[0], &arr[1], &arr[2]); // CHECK-EXEC: {12.00, 8.00, 6.00}
+    TEST_GRADIENT(fn, 3, 4, 9, 10, &arr1[0], &arr1[1], &arr[2]); //CHECK-EXEC: {1530.00, 880.00, -467.00}
+
+}
