8ecfbf81ced48eda96ed73fe42ae7a3f386e3512
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2023-07-03 20:10:35 +0530

    Enhance vector fwd mode to differentiate w.r.t selected params
    After this commit, the user can specify which params it wants to differentiate against.
    
    For ex: if the function is of the form:
    ```cpp
    double f(double x, double y, double z) {
      ...
    }
    ```
    Then `clad::vector_forward_differentiate(f, "x,z")` will produce:
    ```cpp
    void f_dvec_0_2(double x, double y, double z, double *_d_x, double *_d_z) {
      ...
      ...
      {
         *_d_x = ...;
         *_d_z = ...;
         return;
      }
    }
    ```
    
    closes #566

--- test/ForwardMode/VectorModeInterface.C
+++ test/ForwardMode/VectorModeInterface.C
@@ -8,7 +8,7 @@ double f1(double x, double y) {
   return x*y;
 }
 
-// CHECK: void f1_d_all_args(double x, double y, double *_d_x, double *_d_y) {
+// CHECK: void f1_dvec(double x, double y, double *_d_x, double *_d_y) {
 // CHECK-NEXT:   clad::array<double> _d_vector_x = {1., 0.};
 // CHECK-NEXT:   clad::array<double> _d_vector_y = {0., 1.};
 // CHECK-NEXT:   {
@@ -23,9 +23,9 @@ double f2(double x, double y) {
   return x+y;
 }
 
-void f2_d_all_args(double x, double y, double *_d_x, double *_d_y);
+void f2_dvec(double x, double y, double *_d_x, double *_d_y);
 
-// CHECK: void f2_d_all_args(double x, double y, double *_d_x, double *_d_y) {
+// CHECK: void f2_dvec(double x, double y, double *_d_x, double *_d_y) {
 // CHECK-NEXT:   clad::array<double> _d_vector_x = {1., 0.};
 // CHECK-NEXT:   clad::array<double> _d_vector_y = {0., 1.};
 // CHECK-NEXT:   {
@@ -44,7 +44,7 @@ double f_try_catch(double x, double y)
     return 0;
   }
 
-// CHECK: void f_try_catch_d_all_args(double x, double y, double *_d_x, double *_d_y) {
+// CHECK: void f_try_catch_dvec(double x, double y, double *_d_x, double *_d_y) {
 // CHECK-NEXT:   clad::array<double> _d_vector_x = {1., 0.};
 // CHECK-NEXT:   clad::array<double> _d_vector_y = {0., 1.};
 // CHECK-NEXT:    try {
