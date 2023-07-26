b582bfe9aafc10e72fbdf09270cde64532baefe1
daemondzh <69254262+PhrygianGates@users.noreply.github.com>
2023-07-26 09:49:50 +0000

    custom derivative support for operator overload forward mode

--- lib/Differentiator/BaseForwardModeVisitor.cpp
+++ lib/Differentiator/BaseForwardModeVisitor.cpp
@@ -1011,7 +1011,8 @@ StmtDiff BaseForwardModeVisitor::VisitCallExpr(const CallExpr* CE) {
   }
 
   // Try to find a user-defined overloaded derivative.
-  std::string customPushforward = FD->getNameAsString() + "_pushforward";
+  std::string customPushforward =
+      clad::utils::ComputeEffectiveFnName(FD) + "_pushforward";
   Expr* callDiff = m_Builder.BuildCallToCustomDerivativeOrNumericalDiff(
       customPushforward, customDerivativeArgs, getCurrentScope(),
       const_cast<DeclContext*>(FD->getDeclContext()));
