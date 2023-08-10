e83dac31b6abc59539c7fb1092a00992510e91b4
SHOUNAKDAS101 <shounakdas246@gmail.com>
2023-08-10 22:23:18 +0530

    Improved Logic and few test cases changes

--- include/clad/Differentiator/ReverseModeVisitor.h
+++ include/clad/Differentiator/ReverseModeVisitor.h
@@ -14,7 +14,7 @@
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/StmtVisitor.h"
 #include "clang/Sema/Sema.h"
-#include "clang/AST/ParentMapContext.h"  
+
 #include <array>
 #include <memory>
 #include <stack>
@@ -91,10 +91,11 @@ namespace clad {
 
     // Function to Differentiate with Enzyme as Backend
     void DifferentiateWithEnzyme();
-    
+
     // Whether Stmt is Return and not inside any block;
     bool OnlyReturn = false;
-
+    int CCount = 0;
+    
   public:
     using direction = rmv::direction;
     clang::Expr* dfdx() {
