b21f2bd2ceeef32b1f1bcac4bbb3c9c980adcd51
Vassil Vassilev <v.g.vassilev@gmail.com>
2024-01-02 17:14:25 +0000

    Fix strong symbol definitions in Differentiator.h. Add unittests.
    Fixes: #693, Fixes #314.

--- cmake/modules/AddCladBenchmark.cmake
+++ cmake/modules/AddCladBenchmark.cmake
@@ -10,31 +10,87 @@ string(REPLACE "/" "" CURRENT_REPO_COMMIT ${CURRENT_REPO_COMMIT})
 set_property(DIRECTORY APPEND PROPERTY
              CMAKE_CONFIGURE_DEPENDS "${CMAKE_SOURCE_DIR}/.git/HEAD")
 
-
 # Change the default compiler to the clang which we run clad upon.
 set(CMAKE_CXX_COMPILER ${LLVM_TOOLS_BINARY_DIR}/clang)
 
-#----------------------------------------------------------------------------
-# function CB_ADD_GBENCHMARK(<benchmark> source1 source2... LIBRARIES libs)
-#----------------------------------------------------------------------------
-function(CB_ADD_GBENCHMARK benchmark)
-  cmake_parse_arguments(ARG "" "LABEL" "DEPENDS;LIBRARIES" ${ARGN})
-  set(source_files ${ARG_UNPARSED_ARGUMENTS})
-
-  add_executable(${benchmark} ${source_files})
+#-------------------------------------------------------------------------------
+# function ENABLE_CLAD_FOR_EXECUTABLE(<executable>
+#   DEPENDS dependencies...
+#     A list of targets that the executable depends on.
+#   LIBRARIES libraries...
+#     A list of libraries to be linked in. Defaults to stdc++ pthread m.
+# )
+#-------------------------------------------------------------------------------
+function(ENABLE_CLAD_FOR_EXECUTABLE executable)
+  if (NOT TARGET ${executable})
+    message(FATAL_ERROR "'${executable}' is not a valid target.")
+  endif()
 
   # Add the clad plugin
-  target_compile_options(${benchmark} PUBLIC -fplugin=$<TARGET_FILE:clad>)
+  target_compile_options(${executable} PUBLIC -fplugin=$<TARGET_FILE:clad>)
 
   # Debugging. Emitting the derivatives' source code.
-  #target_compile_options(${benchmark} PUBLIC "SHELL:-Xclang -plugin-arg-clad"
+  #target_compile_options(${executable} PUBLIC "SHELL:-Xclang -plugin-arg-clad"
   #  "SHELL: -Xclang -fdump-derived-fn")
 
   # Debugging. Emit llvm IR.
-  #target_compile_options(${benchmark} PUBLIC -S -emit-llvm)
+  #target_compile_options(${executable} PUBLIC -S -emit-llvm)
 
   # Debugging. Optimization misses.
-  #target_compile_options(${benchmark} PUBLIC "SHELL:-Xclang -Rpass-missed=.*inline.*")
+  #target_compile_options(${executable} PUBLIC "SHELL:-Xclang -Rpass-missed=.*inline.*")
+
+  # Clad requires us to link against these libraries.
+  target_link_libraries(${executable} PUBLIC stdc++ pthread m)
+
+  target_include_directories(${executable} PUBLIC ${CMAKE_CURRENT_BINARY_DIR})
+  set_property(TARGET ${executable} PROPERTY RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+
+  add_dependencies(${executable} clad)
+  # If clad.so changes we don't need to relink but to rebuild the source files.
+  # $<TARGET_FILE:clad> does not work for OBJECT_DEPENDS.
+  set (CLAD_SO_PATH "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/clad${CMAKE_SHARED_LIBRARY_SUFFIX}")
+  set_source_files_properties(${source_files} PROPERTY OBJECT_DEPENDS ${CLAD_SO_PATH})
+
+  # Add dependencies to executable
+  if(ARG_DEPENDS)
+    add_dependencies(${executable} ${ARG_DEPENDS})
+  endif(ARG_DEPENDS)
+
+endfunction(ENABLE_CLAD_FOR_EXECUTABLE)
+
+#-------------------------------------------------------------------------------
+# function ADD_CLAD_EXECUTABLE(<executable> sources...
+#   DEPENDS dependencies...
+#     A list of targets that the executable depends on.
+#   LIBRARIES libraries...
+#     A list of libraries to be linked in. Defaults to stdc++ pthread m.
+# )
+#-------------------------------------------------------------------------------
+function(ADD_CLAD_EXECUTABLE executable)
+  cmake_parse_arguments(ARG "" "DEPENDS;LIBRARIES" "" ${ARGN})
+
+  set(source_files ${ARG_UNPARSED_ARGUMENTS})
+
+  add_executable(${executable} ${source_files})
+
+  ENABLE_CLAD_FOR_EXECUTABLE(${executable} ${ARGN})
+
+endfunction(ADD_CLAD_EXECUTABLE)
+
+#-------------------------------------------------------------------------------
+# function CB_ADD_GBENCHMARK(<benchmark> sources
+#   LABEL <short|long>
+#     A label that classifies how much time a benchmark is expected to take.
+#     Short benchmarks time out at 1200 seconds, long at 2400.
+#   DEPENDS dependencies...
+#     A list of targets that the executable depends on.
+#   LIBRARIES libraries...
+#     A list of libraries to be linked in. Defaults to stdc++ pthread m.
+# )
+#-------------------------------------------------------------------------------
+function(CB_ADD_GBENCHMARK benchmark)
+  cmake_parse_arguments(ARG "" "LABEL" "" ${ARGN})
+  ADD_CLAD_EXECUTABLE(${benchmark} ${ARG_UNPARSED_ARGUMENTS})
 
   # Optimize the produced code.
   target_compile_options(${benchmark} PUBLIC -O3)
@@ -42,33 +98,16 @@ function(CB_ADD_GBENCHMARK benchmark)
   # Turn off numerical diff fallback.
   target_compile_definitions(${benchmark} PUBLIC CLAD_NO_NUM_DIFF)
 
-  # Clad requires us to link against these libraries.
-  target_link_libraries(${benchmark} PUBLIC stdc++ pthread m)
-
-  target_include_directories(${benchmark} PUBLIC ${CMAKE_CURRENT_BINARY_DIR} ${GBENCHMARK_INCLUDE_DIR})
-  set_property(TARGET ${benchmark} PROPERTY RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
-
   target_link_libraries(${benchmark} PUBLIC ${ARG_LIBRARIES} gbenchmark)
   if (NOT APPLE)
     target_link_libraries(${benchmark} PUBLIC rt)
   endif()
-
-  add_dependencies(${benchmark} clad)
-  # If clad.so changes we don't need to relink but to rebuild the source files.
-  # $<TARGET_FILE:clad> does not work for OBJECT_DEPENDS.
-  set (CLAD_SO_PATH "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/clad${CMAKE_SHARED_LIBRARY_SUFFIX}")
-  set_source_files_properties(${source_files} PROPERTY OBJECT_DEPENDS ${CLAD_SO_PATH})
 
   set (TIMEOUT_VALUE 1200)
   set (LABEL "short")
   if (ARG_LABEL AND "${ARG_LABEL}" STREQUAL "long")
     set (TIMEOUT_VALUE 2400)
     set (LABEL "long")
-  endif()
-
-  # Add dependencies to benchmark
-  if(ARG_DEPENDS)
-    add_dependencies(${benchmark} ${ARG_DEPENDS})
   endif()
 
   # Add benchmark as a CTest
---
+++ unittests/Basic/MultiIncludeDifferentiatorH.cpp
@@ -0,0 +1,1 @@
+#include "clad/Differentiator/Differentiator.h"
