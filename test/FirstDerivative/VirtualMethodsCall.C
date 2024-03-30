44a7a44765163cba83d0020e181655e0de0b0847
Vassil Vassilev <v.g.vassilev@gmail.com>
2024-03-30 15:40:53 +0000

    Disable tests due broken AST printing in clang-18.
    See llvm/llvm-project#87151

--- test/FirstDerivative/VirtualMethodsCall.C
+++ test/FirstDerivative/VirtualMethodsCall.C
@@ -1,6 +1,8 @@
 // RUN: %cladclang %s -I%S/../../include -oVirtualMethodsCall.out 2>&1 | FileCheck %s
 // RUN: ./VirtualMethodsCall.out | FileCheck -check-prefix=CHECK-EXEC %s
 // XFAIL: asserts
+// Fails on clang-18 due to https://github.com/llvm/llvm-project/issues/87151
+// XFAIL: clang-18
 //CHECK-NOT: {{.*error|warning|note:.*}}
 
 #include "clad/Differentiator/Differentiator.h"
