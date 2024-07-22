2b2af14bd878ec6e74b0a4b16582e88bee4c7e5f
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2024-07-22 17:21:36 +0200

    Try to improve literal types

--- lib/Differentiator/BaseForwardModeVisitor.cpp
+++ lib/Differentiator/BaseForwardModeVisitor.cpp
@@ -1008,8 +1008,11 @@ StmtDiff BaseForwardModeVisitor::VisitDeclRefExpr(const DeclRefExpr* DRE) {
   // If DRE is of type pointer, then the derivative is a null pointer.
   if (clonedDRE->getType()->isPointerType())
     return StmtDiff(clonedDRE, nullptr);
+  QualType literalTy = utils::GetValueType(clonedDRE->getType());
+  if (!literalTy->isRealType())
+    literalTy = m_Context.IntTy;
   return StmtDiff(clonedDRE, ConstantFolder::synthesizeLiteral(
-                                 m_Context.IntTy, m_Context, /*val=*/0));
+                                 literalTy, m_Context, /*val=*/0));
 }
 
 StmtDiff BaseForwardModeVisitor::VisitIntegerLiteral(const IntegerLiteral* IL) {
@@ -1316,8 +1319,12 @@ StmtDiff BaseForwardModeVisitor::VisitUnaryOperator(const UnaryOperator* UnOp) {
   } else if (opKind == UnaryOperatorKind::UO_Deref) {
     if (Expr* dx = diff.getExpr_dx())
       return StmtDiff(op, BuildOp(opKind, dx));
-    return StmtDiff(op, ConstantFolder::synthesizeLiteral(
-                            m_Context.IntTy, m_Context, /*val=*/0));
+    QualType literalTy =
+        utils::GetValueType(UnOp->getSubExpr()->getType()->getPointeeType());
+    if (!literalTy->isRealType())
+      literalTy = m_Context.IntTy;
+    return StmtDiff(
+        op, ConstantFolder::synthesizeLiteral(literalTy, m_Context, /*val=*/0));
   } else if (opKind == UnaryOperatorKind::UO_AddrOf) {
     return StmtDiff(op, BuildOp(opKind, diff.getExpr_dx()));
   } else if (opKind == UnaryOperatorKind::UO_LNot) {
