7fb94c6f275440d990dd6eb65bcdd36f1f5f734d
petro.zarytskyi <petro.zarytskyi@gmail.com>
2024-08-07 18:21:37 +0300

    Add a special case to differentiate begin/end calls

--- lib/Differentiator/ReverseModeVisitor.cpp
+++ lib/Differentiator/ReverseModeVisitor.cpp
@@ -1573,6 +1573,29 @@ Expr* getArraySizeExpr(const ArrayType* AT, ASTContext& context,
       return StmtDiff(Call, zero);
     }
 
+    // begin and end are common enough to have a more efficient and nice-looking
+    // special case. Instead of _forw and a useless _pullback functions, we can
+    // express the result in terms of the same std::begin / std::end. Note:
+    // since std::initializer_list is replaced with clad::array, this is the
+    // simplest way to support begin/end functions of the former and not deal
+    // with the type mismatch.
+    std::string FDName = FD->getNameAsString();
+    if (FDName == "begin" || FDName == "end") {
+      const Expr* arg = nullptr;
+      if (const auto* MCE = dyn_cast<CXXMemberCallExpr>(CE))
+        arg = MCE->getImplicitObjectArgument();
+      else
+        arg = CE->getArg(0);
+      if (const auto* CXXCE = dyn_cast<CXXConstructExpr>(arg))
+        arg = CXXCE->getArg(0);
+      StmtDiff argDiff = Visit(arg);
+      llvm::SmallVector<Expr*, 1> params{argDiff.getExpr()};
+      llvm::SmallVector<Expr*, 1> paramsDiff{argDiff.getExpr_dx()};
+      Expr* call = GetFunctionCall(FDName, "std", params);
+      Expr* callDiff = GetFunctionCall(FDName, "std", paramsDiff);
+      return {call, callDiff};
+    }
+
     auto NArgs = FD->getNumParams();
     // If the function has no args and is not a member function call then we
     // assume that it is not related to independent variables and does not
