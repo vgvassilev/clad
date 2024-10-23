75d9530a232e90809f2acc7375e3f0a73377bd5c
Mihail Mihov <mihovmihailp+github@gmail.com>
2024-10-24 00:11:10 +0300

    Fix ForwardMode/NotEnoughArgError.C

--- test/ForwardMode/NotEnoughArgError.C
+++ test/ForwardMode/NotEnoughArgError.C
@@ -13,8 +13,8 @@ int main () {
   // expected-error@clad/Differentiator/Differentiator.h:* {{too few arguments to function call, expected 2, have 1}}
   // expected-note@clad/Differentiator/Differentiator.h:* {{in instantiation of function template specialization 'clad::execute_with_default_args<false, double, double (*)(double, double), int, double, true>' requested here}}
 #if __clang_major__ < 16
-  // expected-note@clad/Differentiator/Differentiator.h:* {{in instantiation of function template specialization 'clad::CladFunction<double (*)(double, double), clad::NoObject, false>::execute_helper<double (*)(double, double), int>' requested here}}
-  // expected-note@NotEnoughArgError.C:12 {{in instantiation of function template specialization 'clad::CladFunction<double (*)(double, double), clad::NoObject, false>::execute<int, double (*)(double, double)>' requested here}}
+  // expected-note@clad/Differentiator/Differentiator.h:* {{in instantiation of function template specialization 'clad::CladFunction<double (*)(double, double), clad::NoObject, false, false>::execute_helper<double (*)(double, double), int>' requested here}}
+  // expected-note@NotEnoughArgError.C:12 {{in instantiation of function template specialization 'clad::CladFunction<double (*)(double, double), clad::NoObject, false, false>::execute<int, double (*)(double, double)>' requested here}}
 #else
   // expected-note@clad/Differentiator/Differentiator.h:* {{in instantiation of function template specialization 'clad::CladFunction<double (*)(double, double), clad::NoObject>::execute_helper<double (*)(double, double), int>' requested here}}
   // expected-note@NotEnoughArgError.C:12 {{in instantiation of function template specialization 'clad::CladFunction<double (*)(double, double), clad::NoObject>::execute<int, double (*)(double, double)>' requested here}}
