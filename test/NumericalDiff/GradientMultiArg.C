b9d77fd3acad08e384cb904e4cae8f4ffd24ec5a
Abdelrhman Elrawy <abdelrhman.elrawy1@gmail.com>
2025-02-05 15:33:20 -0500

    Add tests for the new functions

--- test/NumericalDiff/GradientMultiArg.C
+++ test/NumericalDiff/GradientMultiArg.C
@@ -8,8 +8,8 @@
 #include <cmath>
 #include <algorithm>
 
-double test_1(double x, double y){
-  return std::hypot(x, y); // expected-warning {{function 'hypot' was not differentiated}}
+double test_1(double x, double y) {
+  return std::fmod(x, y); // expected-warning {{function 'fmod' was not differentiated}}
   // expected-note@12 {{falling back to numerical differentiation}}
 }
 // CHECK: void test_1_grad(double x, double y, double *_d_x, double *_d_y) {
@@ -17,7 +17,7 @@ double test_1(double x, double y){
 // CHECK-NEXT:         double _r0 = 0.;
 // CHECK-NEXT:         double _r1 = 0.;
 // CHECK-NEXT:         double _grad0[2] = {0};
-// CHECK-NEXT:         numerical_diff::central_difference(std::hypot, _grad0, 0, x, y);
+// CHECK-NEXT:         numerical_diff::central_difference(std::fmod, _grad0, 0, x, y);
 // CHECK-NEXT:         _r0 += 1 * _grad0[0];
 // CHECK-NEXT:         _r1 += 1 * _grad0[1];
 // CHECK-NEXT:         *_d_x += _r0;
@@ -30,6 +30,6 @@ int main(){
     auto df = clad::gradient(test_1);
     double dx = 0, dy = 0;
     df.execute(3, 4, &dx, &dy);
-    printf("Result is = %f\n", dx); // CHECK-EXEC: Result is = 0.600000
-    printf("Result is = %f\n", dy); // CHECK-EXEC: Result is = 0.800000
+    printf("Result is = %f\n", dx); // CHECK-EXEC: Result is = 1.000000
+    printf("Result is = %f\n", dy); // CHECK-EXEC: Result is = 0.000000
 }
