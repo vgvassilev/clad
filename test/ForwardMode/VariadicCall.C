4909f69253328d049785a83548c51b131a23312e
kchristin <christinakoutsou22@gmail.com>
2025-02-21 15:45:41 +0200

    Update variadic test with non-template variadic function call

--- test/ForwardMode/VariadicCall.C
+++ test/ForwardMode/VariadicCall.C
@@ -3,7 +3,8 @@
 
 #include "clad/Differentiator/Differentiator.h"
 
-#include "../TestUtils.h"
+// CHECK: warning: function 'printf' was not differentiated because clad failed to differentiate it and no suitable overload was found in namespace 'custom_derivatives'
+// CHECK: note: fallback to numerical differentiation is disabled by the 'CLAD_NO_NUM_DIFF' macro; considering 'printf' as 0   
 
 template <typename... T>
 double fn1(double x, T... y) {
@@ -11,12 +12,14 @@ double fn1(double x, T... y) {
 }
 
 double fn2(double x, double y) {
+    printf("x is %f, y is %f\n", x, y);
     return fn1(x, y);
 }
 
 // CHECK: double fn2_darg0(double x, double y) {
 // CHECK-NEXT:     double _d_x = 1;
 // CHECK-NEXT:     double _d_y = 0;
+// CHECK-NEXT:     printf("x is %f, y is %f\n", x, y);
 // CHECK-NEXT:     clad::ValueAndPushforward<double, double> _t0 = fn1_pushforward(x, y, _d_x, _d_y);
 // CHECK-NEXT:     return _t0.pushforward;
 // CHECK-NEXT: }
@@ -32,13 +35,13 @@ double fn3(double x, double y) {
 // CHECK-NEXT:     return _t0.pushforward;
 // CHECK-NEXT: }
 
-
 int main() {
-  INIT_DIFFERENTIATE(fn2, "x");
-  TEST_DIFFERENTIATE(fn2, 1.0, 2.0); // CHECK-EXEC: {2.00}
-  INIT_DIFFERENTIATE(fn3, "x");
-  TEST_DIFFERENTIATE(fn3, 1.0, 2.0); // CHECK-EXEC: {4.00}
-  return 0;
+    auto d_fn2 = clad::differentiate(fn2, "x");
+    printf("{%.2f}\n", d_fn2.execute(1.0, 2.0)); // CHECK-EXEC: x is 1.000000, y is 2.000000
+    // CHECK-EXEC: {2.00}
+    auto d_fn3 = clad::differentiate(fn3, "x");
+    printf("{%.2f}\n", d_fn3.execute(1.0, 2.0)); // CHECK-EXEC: {4.00}
+    return 0;
 }
 
 // CHECK: clad::ValueAndPushforward<double, double> fn1_pushforward(double x, double y, double _d_x, double _d_y) {
