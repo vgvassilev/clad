add07827d1414e2dd5e63fa54ef346ec18d221b8
Abdullah Al Sharji <asharji1828@gmail.com>
2025-03-27 13:15:30 +0400

    Added tests for Uninitialized structures delcared in loops

---
+++ test/Misc/StructsInLoops.C
@@ -0,0 +1,49 @@
+// RUN: %cladclang %s -o %t
+// RUN: %t | FileCheck %s
+
+#include "clad/Differentiator/Differentiator.h"
+#include <stdio.h>
+
+typedef struct {
+    double val;
+} Struct;
+
+double fn1(double a) {
+    double result = 0;
+    for (int i=0;i<1;i++){
+        Struct s;
+        s.val += 2;
+        result += s.val * a;
+    }
+    return result;
+}
+
+double fn2 (double a) {
+    double result = 0;
+    int i = 1;
+    while (i--) {
+        Struct s1;
+        Struct s2;
+        s2.val = s1.val + 2;
+        result += s2.val * a * a;
+    }
+    return result;
+}
+
+int main() {
+    auto grad = clad::gradient(fn1);
+    auto grad2 = clad::gradient(fn2);
+
+    double a = 2.0;
+    double b = 3.0;
+
+    double d_a = 0;
+    grad.execute(a, &d_a);
+    printf("fn1 derivative: %.1f\n", d_a); // CHECK: fn1 derivative: 2.0
+    
+    double d_b = 0;
+    grad2.execute(b, &d_b);
+    printf("fn2 derivative: %.1f\n", d_b); // CHECK: fn2 derivative: 12.0
+    
+    return 0;
+}
