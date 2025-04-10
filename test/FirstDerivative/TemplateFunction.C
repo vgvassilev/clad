7b0ca5f3f059563822060727572e2ffe45e6d998
Vassil Vassilev <v.g.vassilev@gmail.com>
2025-04-10 16:07:14 +0000

    Revert "Add information about function template specialization to `DerivativeBuilder::cloneFunction` (#1277)"
    This reverts commit cb04bdabccd89ebeafbff3b2f8a7ce5491d7354b.

--- test/FirstDerivative/TemplateFunction.C
+++ test/FirstDerivative/TemplateFunction.C
@@ -21,55 +21,55 @@ int main () {
   int x;
 
   clad::differentiate(simple_return<int>, 0);
-  // CHECK: template<> int simple_return_darg0<int>(int x) {
+  // CHECK: int simple_return_darg0(int x) {
   // CHECK-NEXT: int _d_x = 1;
   // CHECK-NEXT: return _d_x;
   // CHECK-NEXT: }
 
   clad::differentiate(simple_return<float>, 0);
-  // CHECK: template<> float simple_return_darg0<float>(float x) {
+  // CHECK: float simple_return_darg0(float x) {
   // CHECK-NEXT: float _d_x = 1;
   // CHECK-NEXT: return _d_x;
   // CHECK-NEXT: }
 
   clad::differentiate(simple_return<double>, 0);
-  // CHECK: template<> double simple_return_darg0<double>(double x) {
+  // CHECK: double simple_return_darg0(double x) {
   // CHECK-NEXT: double _d_x = 1;
   // CHECK-NEXT: return _d_x;
   // CHECK-NEXT: }
 
   clad::differentiate(addition<int>, 0);
-  // CHECK: template<> int addition_darg0<int>(int x) {
+  // CHECK: int addition_darg0(int x) {
   // CHECK-NEXT: int _d_x = 1;
   // CHECK-NEXT: return _d_x + _d_x;
   // CHECK-NEXT: }
 
   clad::differentiate(addition<float>, 0);
-  // CHECK: template<> float addition_darg0<float>(float x) {
+  // CHECK: float addition_darg0(float x) {
   // CHECK-NEXT: float _d_x = 1;
   // CHECK-NEXT: return _d_x + _d_x;
   // CHECK-NEXT: }
 
   clad::differentiate(addition<double>, 0);
-  // CHECK: template<> double addition_darg0<double>(double x) {
+  // CHECK: double addition_darg0(double x) {
   // CHECK-NEXT: double _d_x = 1;
   // CHECK-NEXT: return _d_x + _d_x;
   // CHECK-NEXT: }
 
   clad::differentiate(multiplication<int>, 0);
-  // CHECK: template<> int multiplication_darg0<int>(int x) {
+  // CHECK: int multiplication_darg0(int x) {
   // CHECK-NEXT: int _d_x = 1;
   // CHECK-NEXT: return _d_x * x + x * _d_x;
   // CHECK-NEXT: }
 
   clad::differentiate(multiplication<float>, 0);
-  // CHECK: template<> float multiplication_darg0<float>(float x) {
+  // CHECK: float multiplication_darg0(float x) {
   // CHECK-NEXT: float _d_x = 1;
   // CHECK-NEXT: return _d_x * x + x * _d_x;
   // CHECK-NEXT: }
 
   clad::differentiate(multiplication<double>, 0);
-  // CHECK: template<> double multiplication_darg0<double>(double x) {
+  // CHECK: double multiplication_darg0(double x) {
   // CHECK: double _d_x = 1;
   // CHECK-NEXT: return _d_x * x + x * _d_x;
   // CHECK-NEXT: }
