181333004f5073cb82d4c3ba41a43294b5cd987f
kchristin <christinakoutsou22@gmail.com>
2025-06-16 14:54:07 +0300

    Fix format

--- include/clad/Differentiator/BuiltinDerivatives.h
+++ include/clad/Differentiator/BuiltinDerivatives.h
@@ -69,8 +69,8 @@ cudaMalloc_pushforward(T** devPtr, size_t sz, T** d_devPtr, size_t d_sz)
 
 ValueAndPushforward<cudaError_t, cudaError_t>
 cudaMemcpy_pushforward(void* destPtr, const void* srcPtr, size_t count,
-                       cudaMemcpyKind kind, void* d_destPtr, const void* d_srcPtr,
-                       size_t d_count) {
+                       cudaMemcpyKind kind, void* d_destPtr,
+                       const void* d_srcPtr, size_t d_count) {
   return {cudaMemcpy(destPtr, srcPtr, count, kind),
           cudaMemcpy(d_destPtr, d_srcPtr, count, kind)};
 }
