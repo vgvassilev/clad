47045bb3b7aa840210be8e216bcc90be70244d75
petro.zarytskyi <petro.zarytskyi@gmail.com>
2025-12-10 11:17:40 +0100

    Add tests for numerical differentiation

---
+++ test/Gradient/NumDiff.C
@@ -0,0 +1,54 @@
+// RUN: %cladnumdiffclang %s %S/NumDiffDefs.C -I%S/../../include -oNumDiff.out -Xclang -verify 2>&1 | FileCheck -check-prefix=CHECK %s
+// RUN: ./NumDiff.out | %filecheck_exec %s
+// XFAIL: valgrind
+
+#include "clad/Differentiator/Differentiator.h"
+#include "../TestUtils.h"
+
+double single_arg(double x);
+
+double f1(double x, double y) {
+  return single_arg(x * y); // expected-warning {{function 'single_arg' was not differentiated because clad failed to differentiate it and no suitable overload was found in namespace 'custom_derivatives'}}
+  // expected-note@11 {{falling back to numerical differentiation for 'single_arg'}}
+}
+
+//CHECK: void f1_grad(double x, double y, double *_d_x, double *_d_y) {
+//CHECK-NEXT:     {
+//CHECK-NEXT:         double _r0 = 0.;
+//CHECK-NEXT:         _r0 += 1 * numerical_diff::forward_central_difference(single_arg, x * y, 0, 0, x * y);
+//CHECK-NEXT:         *_d_x += _r0 * y;
+//CHECK-NEXT:         *_d_y += x * _r0;
+//CHECK-NEXT:     }
+//CHECK-NEXT: }
+
+double multi_arg(double x, double y);
+
+double f2(double x, double y) {
+  return multi_arg(x, y); // expected-warning {{function 'multi_arg' was not differentiated because clad failed to differentiate it and no suitable overload was found in namespace 'custom_derivatives'}}
+  // expected-note@27 {{falling back to numerical differentiation for 'multi_arg'}}
+}
+
+
+//CHECK: void f2_grad(double x, double y, double *_d_x, double *_d_y) {
+//CHECK-NEXT:     {
+//CHECK-NEXT:         double _r0 = 0.;
+//CHECK-NEXT:         double _r1 = 0.;
+//CHECK-NEXT:         double _grad0[2] = {0};
+//CHECK-NEXT:         numerical_diff::central_difference(multi_arg, _grad0, 0, x, y);
+//CHECK-NEXT:         _r0 += 1 * _grad0[0];
+//CHECK-NEXT:         _r1 += 1 * _grad0[1];
+//CHECK-NEXT:         *_d_x += _r0;
+//CHECK-NEXT:         *_d_y += _r1;
+//CHECK-NEXT:     }
+//CHECK-NEXT: }
+
+int main() {
+  double dx = 0, dy = 0;
+  INIT_GRADIENT(f1);
+  TEST_GRADIENT(f1, /*numOfDerivativeArgs=*/2, 4, 5, &dx, &dy); // CHECK-EXEC: {10.00, 8.00}
+
+  dx = 0; dy = 0;
+  INIT_GRADIENT(f2);
+  TEST_GRADIENT(f2, /*numOfDerivativeArgs=*/2, 2, 3, &dx, &dy); // CHECK-EXEC: {1.00, 1.00}
+  return 0;
+}
---
+++ test/Gradient/NumDiffDefs.C
@@ -0,0 +1,9 @@
+// expected-no-diagnostics
+// RUN:
+double single_arg(double x) {
+    return 2 * x;
+}
+
+double multi_arg(double x, double y) {
+    return x + y;
+}
