9b8e961f1cd55d42c51455ada57fadd898c24af2
Ayush <ayushssp74@gmail.com>
2025-12-11 02:27:43 +0000

    [Derivatives] Add support for std::comp_ellint_1
    Implemented the derivative of the Complete Elliptic Integral of the First Kind (std::comp_ellint_1) using the standard formula involving std::comp_ellint_2. Verified against numerical differentiation.

--- include/clad/Differentiator/BuiltinDerivatives.h
+++ include/clad/Differentiator/BuiltinDerivatives.h
@@ -1059,6 +1059,32 @@ CUDA_HOST_DEVICE void hypot_pullback(T x, T y, U d_z, T* d_x, T* d_y) {
   *d_y += (y / h) * d_z;
 }
 
+// -------------------------------------------------------------------------
+// [Ayush GSoC 2026 Contribution]
+// Feature: Added support for Complete Elliptic Integral of the First Kind
+// Target: std::comp_ellint_1(k)
+// Formula: dK/dk = (E(k) - (1-k^2)*K(k)) / (k * (1-k^2))
+// -------------------------------------------------------------------------
+template <typename T, typename dT>
+CUDA_HOST_DEVICE ValueAndPushforward<T, dT> comp_ellint_1_pushforward(T k, dT d_k) {
+  // [Ayush]: Calculate K(k) and E(k) using C++17 std math
+  T K = ::std::comp_ellint_1(k);
+  T E = ::std::comp_ellint_2(k);
+
+  // [Ayush]: Helper variables for the derivative formula
+  T one = 1.0;
+  T k_sq = k * k;
+  T term = one - k_sq;
+
+  // [Ayush]: Calculate Derivative
+  // Formula: (E - (1-k^2)K) / (k * (1-k^2))
+  T numerator = E - (term * K);
+  T denominator = k * term;
+
+  // [Ayush]: Return value and pushforward (chain rule applied)
+  return {K, (numerator / denominator) * d_k};
+}
+
 } // namespace std
 
 CUDA_HOST_DEVICE inline ValueAndPushforward<float, float>
@@ -1323,6 +1349,7 @@ using std::min_pushforward;
 using std::pow_pullback;
 using std::pow_pushforward;
 using std::sqrt_pushforward;
+using std::comp_ellint_1_pushforward;
 
 namespace class_functions {
 template <typename T, typename U>
