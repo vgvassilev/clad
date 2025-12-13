f8517b170fbe05494372d122160aea6e5bc00bb4
Aditi Rawat <aditirawat@Aditis-Air.lan>
2025-12-13 10:29:17 +0530

    Removed unsupported flags by Clang

--- cmake/ClangBenchmark.cmake
+++ cmake/ClangBenchmark.cmake
@@ -1,15 +1,6 @@
-function(clad_apply_test_compiler target)
-    target_compile_options(${target} PRIVATE
-        -Wno-class-memaccess
-        -Wno-undefined-inline
-        -fno-lifetime-dse
-    )
-
-    target_compile_options(${target} PRIVATE
-        $<$<NOT:$<CXX_COMPILER_ID:Clang>>:-flags>
-    )
-
-    target_compile_definitions(${target} PRIVATE CLAD_TESTING=1)
+function(clad_apply_benchmark_compiler target)
+    target_compile_options(
+    ${target} PRIVATE  -Wno-undefined-inline)
 
     set_property(TARGET ${target} PROPERTY CXX_COMPILER ${LLVM_TOOLS_BINARY_DIR}/clang++)
 endfunction()
