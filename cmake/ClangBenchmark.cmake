4f77228a8cd3bdfacb126d39c4aae7c1a6dc952b
Aditi Rawat <aditirawat@Aditis-Air.lan>
2025-12-15 14:18:09 +0530

    Added function definition in benchmark

--- cmake/ClangBenchmark.cmake
+++
@@ -1,6 +0,0 @@
-function(clad_apply_benchmark_compiler target)
-    target_compile_options(
-    ${target} PRIVATE  -Wno-undefined-inline)
-
-    set_property(TARGET ${target} PROPERTY CXX_COMPILER ${LLVM_TOOLS_BINARY_DIR}/clang++)
-endfunction()
