2736d290edea73924485941e3f1aae31c097ae7d
Ayush <ayushssp74@gmail.com>
2026-01-12 09:31:47 +0000

    Test: Add domain comments and verify comp_ellint_3 numerically

--- test/Features/stl-cmath.cpp
+++ test/Features/stl-cmath.cpp
@@ -306,6 +306,7 @@ DEFINE_FUNCTIONS(erf)  // x in (-inf,+inf)
 inline float comp_ellint_1f(float x) { return std::comp_ellint_1(x); }
 inline long double comp_ellint_1l(long double x) { return std::comp_ellint_1(x); }
 
+// Domain: k in (-1, 1)
 DEFINE_FUNCTIONS(comp_ellint_1)
 CHECK_ALL_RANGE(comp_ellint_1, -0.9, 0.9);
 
@@ -313,24 +314,35 @@ CHECK_ALL_RANGE(comp_ellint_1, -0.9, 0.9);
 inline float comp_ellint_2f(float x) { return std::comp_ellint_2(x); }
 inline long double comp_ellint_2l(long double x) { return std::comp_ellint_2(x); }
 
+// Domain: k in (-1, 1)
 DEFINE_FUNCTIONS(comp_ellint_2)
 CHECK_ALL_RANGE(comp_ellint_2, -0.9, 0.9);
 
 // comp_ellint_3 takes 2 arguments, so we test it manually (macro doesn't support it)
 {
   double k = 0.5, nu = 0.3;
-  
+  double h = 1e-8;
+  double tol = 1e-5;
+
   // Test differentiation w.r.t 'k' (Arg 0)
   auto d_ellint3_k = clad::differentiate(std::comp_ellint_3, 0);
-  double res_k = d_ellint3_k.execute(k, nu);
+  double sym_k = d_ellint3_k.execute(k, nu);
+  // Numerical approximation: (f(k+h) - f(k-h)) / 2h
+  double num_k = (std::comp_ellint_3(k + h, nu) - std::comp_ellint_3(k - h, nu)) / (2 * h);
+  
+  if (std::abs(sym_k - num_k) > tol) {
+    return 1;
+  }
 
   // Test differentiation w.r.t 'nu' (Arg 1)
   auto d_ellint3_nu = clad::differentiate(std::comp_ellint_3, 1);
-  double res_nu = d_ellint3_nu.execute(k, nu);
-  
-  // Simple check to ensure we got numbers back (prevents unused var warning)
-  (void)res_k; 
-  (void)res_nu;
+  double sym_nu = d_ellint3_nu.execute(k, nu);
+  // Numerical approximation: (f(nu+h) - f(nu-h)) / 2h
+  double num_nu = (std::comp_ellint_3(k, nu + h) - std::comp_ellint_3(k, nu - h)) / (2 * h);
+
+  if (std::abs(sym_nu - num_nu) > tol) {
+    return 1;
+  }
 }
 //
 
