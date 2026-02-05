e05f5d5f91337c96ecdb71f7085df3c9a8347e35
Ayush <ayushssp74@gmail.com>
2026-02-05 15:08:36 +0000

    Fix: Update regression test to use clad::gradient and correct signatures

--- test/Regressions/issue-1690.cpp
+++ test/Regressions/issue-1690.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -fplugin=%clad_plugin -std=c++17 %s -o %t
+// RUN: %clang -fplugin=%clad_plugin -std=c++17 %s -o %t
 // RUN: %t | FileCheck %s
 
 #include "clad/Differentiator/Differentiator.h"
@@ -8,10 +8,14 @@ double test_func(double* x) { return x[0]; }
 
 int main() {
   double arr[2] = {1.0, 2.0};
-  auto d_test = clad::differentiate(test_func, "x");
+  double grads[2] = {0.0, 0.0}; // Buffer for the gradient output
 
-  // This used to crash/error. Now it should implicitly decay to pointer and run.
-  d_test.execute(arr);
+  // Use clad::gradient (Reverse Mode), which supports array inputs/outputs
+  auto d_test = clad::gradient(test_func);
+
+  // This used to crash/error when passing raw arrays. 
+  // Now it should implicitly decay arrays to pointers and execute successfully.
+  d_test.execute(arr, grads);
 
   printf("Success\n");
   // CHECK: Success
