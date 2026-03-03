4bfed253ff414073c5fb9354c56a4b8492fcfd37
Arnav Kapoor <kapoorarnav43@gmail.com>
2026-03-03 14:10:18 +0530

    Add header-only tape and pullback runtime

--- test/Misc/TapeAndPullbackHeaderOnly.C
+++ test/Misc/TapeAndPullbackHeaderOnly.C
@@ -5,6 +5,8 @@
 
 #include <cmath>
 #include <cstdio>
+#include <thread>
+#include <vector>
 
 int main() {
   clad::tape<double> t = {};
@@ -24,6 +26,34 @@ int main() {
   if (std::fabs(v - 3.0) > 1e-12) {
     std::printf("unexpected tape value: %f\n", v);
     return 2;
+  }
+
+  double arr[3] = {1.0, 2.0, 3.0};
+  clad::tape<double[3]> arrTape = {};
+  clad::push(arrTape, arr);
+  auto& arrBack = clad::back(arrTape);
+  if (std::fabs(arrBack[0] - 1.0) > 1e-12 ||
+      std::fabs(arrBack[1] - 2.0) > 1e-12 ||
+      std::fabs(arrBack[2] - 3.0) > 1e-12) {
+    std::printf("unexpected array tape values\n");
+    return 3;
+  }
+  clad::pop(arrTape);
+
+  clad::tape<int, 64, 1024, true> mtTape = {};
+  std::vector<std::thread> workers;
+  for (int i = 0; i < 4; ++i) {
+    workers.emplace_back([&]() {
+      for (int j = 0; j < 1000; ++j)
+        clad::push<int>(mtTape, 1);
+    });
+  }
+  for (auto& worker : workers)
+    worker.join();
+
+  if (mtTape.size() != 4000) {
+    std::printf("unexpected multithreaded tape size: %zu\n", mtTape.size());
+    return 4;
   }
 
   return 0;
