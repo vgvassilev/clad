35c5d2c3c5accbe8bb8719b786d25c3021f1a5bf
fogsong233 <fogsong233@gmail.com>
2026-06-10 12:52:45 +0800

    format

--- lib/Differentiator/ReverseModeVisitor.cpp
+++ lib/Differentiator/ReverseModeVisitor.cpp
@@ -2205,8 +2205,7 @@ Expr* ReverseModeVisitor::getStdInitListSizeExpr(const Expr* E) {
           !m_DiffReq.CallContext || hasDynamicNonDiffParams ||
           FD->getNameAsString() == "cudaMemcpy" || hasMemberForwPass;
       if (missingRefReturnPullback || !m_DiffReq.CallContext ||
-          hasDynamicNonDiffParams ||
-          FD->getNameAsString() == "cudaMemcpy") {
+          hasDynamicNonDiffParams || FD->getNameAsString() == "cudaMemcpy") {
         pullbackFD = nullptr;
         // Try to find it in builtin derivatives.
         std::string customPullback = pullbackRequest.ComputeDerivativeName();
